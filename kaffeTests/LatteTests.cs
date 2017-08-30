10119fa31c7ef50468f6419fe8c24b6e58edf106
Martin Kierkegaard <mark@easj.dk>
2017-08-30 09:58:16 +0200

    tilføjet test af Latte pris

---
+++ kaffeTests/LatteTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using kaffe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaffe.Tests
+{
+    [TestClass()]
+    public class LatteTests
+    {
+        [TestMethod()]
+        public void prisTestLatte()
+        {
+            //Arrange
+            var latte = new Latte();
+
+            //Act
+            int pris = latte.pris();
+
+            //assert
+            Assert.AreEqual(40, pris);
+
+        }
+    }
+}
